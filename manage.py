f77437a696a7a54d22e0f74757d959ceec3ed197
17561916530 <hxz669828@163.com>
2018-06-29 16:09:57 +0800

    k

--- manage.py
+++ manage.py
@@ -9,11 +9,13 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEATDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFTCATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -21,7 +23,7 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 
 
-class Rola(db.Model):
+class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
@@ -42,7 +44,7 @@ class User(db.Model):
 
 
 class NameForm(FlaskForm):
-    name = StringField('what is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
 
