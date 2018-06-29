c02758b26dfcdec49d63182d9d7910fb73601dc2
17561916530 <hxz669828@163.com>
2018-06-29 10:56:38 +0800

    g

--- second.py
+++ second.py
@@ -1,7 +1,9 @@
+from flask_script import Manager
 from alembic.autogenerate import render
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
+manager=Manager(app)
 
 @app.route('/')
 def index():
@@ -24,8 +26,18 @@ def func():
     comments = ['ccsdhadfh', 'ccxuxdfu', 'chdjsdfdhf', 'csfddhhfu']
     return render_template('comment_test.html', comments=comments)
 
+@app.route('/in/<hah>/')
+def inherit(hah):
+    return render_template('base1_inherit.html',hah=hah)
 
+@app.route('/check/<name>/')
+def checkqqq(name):
+    urls=url_for('checkqqq',name="llll",page=2,pa=3,_external=True)
+    return render_template('hello.html',urls=urls)
 
+@manager.command
+def home():
+    return "aaaa"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
