866934dfff2470e480c8c19e2d15e03385622623
17561916530 <hxz669828@163.com>
2018-07-01 16:01:01 +0800

    a

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 import os
+from threading import Thread
+
 from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -7,6 +9,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,11 +20,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER']='smtp.163.com'
+app.config['MAIL_PORT']=25
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']='hxz669828@163.com'
+app.config['MAIL_PASSWORD']='laopo1314'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX']='[Flasky]'
+app.config['FLASKY_MAIL_SENDER']='Flasky Admin <hxz669828@163.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -42,6 +57,19 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+def send_asyns_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to,subject,template,**kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' '+subject,sender = app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    msg.body=render_template(template+".txt",**kwargs)
+    msg.html=render_template(template+".html",**kwargs)
+    thr=Thread(target=send_asyns_email,args=[app,msg])
+    thr.start()
+    return thr
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
@@ -67,12 +95,16 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
+
 
 
 if __name__ == '__main__':
